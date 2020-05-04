d0182f8cb5c444aaa14ebb5fd23dd9d6d69bda5a
Ben Keener <keener4christ@gmail.com>
2020-05-04 16:15:56 +0000

    Completed task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This file uses a testing API to get data"""
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__" and len(sys.argv) >= 2:
+    gid = sys.argv[1]
+    req = requests.get("http://jsonplaceholder.typicode.com/" +
+                       "users/{}/todos".format(gid))
+    data = req.json()
+
+    rname = requests.get("http://jsonplaceholder.typicode.com/" +
+                         "users/{}".format(gid))
+    name = rname.json()["name"]
+    doneTasks = [task for task in data if task["completed"] is True]
+    print("Employee {} is done with ".format(name) +
+          "tasks({}/{}):".format(len(doneTasks), len(data)))
+
+    for todo in doneTasks:
+        print("\t {}".format(todo["title"]))
