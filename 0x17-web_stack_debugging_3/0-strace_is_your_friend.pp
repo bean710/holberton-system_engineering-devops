02a33d3e8e40a747094bbdf980cad8419f8fef37
Ben Keener <keener4christ@gmail.com>
2020-05-20 23:14:59 +0000

    Fixed formatting

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 #Replaces a typo
-exec { 'Runs sed to fix typo'
-  command => 'sed -i 's/phpp/php/g' /var/www/html/wp-settings.php'
+file { 'Runs sed to fix typo'
+  command => 'sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php',
+  path    => '/var/'
 }
